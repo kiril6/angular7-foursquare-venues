d952bad5552a4fb2bfb58491f0615a0b8d34e6a5
MUSALA\kiril.delovski <kiril.delovski@musala.com>
2017-09-29 13:52:19 +0200

    version one of the code

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Response } from '@angular/http';
+
+import { DataStorageService } from '../shared/data-storage.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+  constructor(private dataStorageService: DataStorageService) {}
+
+  onSaveData() {
+    this.dataStorageService.storeRecipes()
+      .subscribe(
+        (response: Response) => {
+          console.log(response);
+        }
+      );
+  }
+
+  onFetchData() {
+    this.dataStorageService.getRecipes();
+  }
+}
